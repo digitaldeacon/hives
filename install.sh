1c872f3057c78bded0839e78aba25c79f8c59e84
Paul Walger <metaxy@walger.name>
2015-07-11 18:14:35 +0200

    update

--- install.sh
+++ install.sh
@@ -5,13 +5,15 @@ mkdir ${base}/data/git
 mkdir ${base}/data/subdomains
 mkdir ${base}/data/dist
 mkdir ${base}/data/code
-
+cd ${base}/data/code
+git init
 cd ${base}/data/git
 git init --bare
 git remote add origin ${base}/data/code
 cd ${base}
 cp ${base}/hooks/post-receive ${base}/data/git/hooks
 chmod +x ${base}/data/git/hooks/post-receive
+
 
 if [ ! -f config.json ]
 then
