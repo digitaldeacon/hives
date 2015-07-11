2913445d88ca5dc7f81e7a79d19d9f596b688a1b
Paul Walger <metaxy@walger.name>
2015-07-11 19:41:50 +0200

    update

--- update.rb
+++ update.rb
@@ -1,4 +1,4 @@
 require_relative 'common'
 #FileUtils.cp_r(path_dist(), path_subdomain("static"))
-
-exe("cd #{$path}/data/code && git pull origin master")
+puts "Update Scripts"
+#exea("cd #{$path}/data/code && git pull origin master")
