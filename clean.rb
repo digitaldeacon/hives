7d7c41992825f2f04477f04f4e175a34b6c87545
Paul Walger <metaxy@walger.name>
2015-07-11 14:48:01 +0200

    update

---
+++ clean.rb
@@ -0,0 +1,5 @@
+require 'json'
+require 'fileutils'
+require_relative 'common'
+
+remove_subdomain_plesk("static")
--- common.rb
+++ common.rb
@@ -12,3 +12,14 @@ end
 def path_dist()
   "#{$path}/data/dist"
 end
+
+def create_subdomain_plesk(subdomain)
+  domain = "memberhive.com"
+  path = path_subdomain(subdomain)
+  `sudo /usr/local/psa/bin/subdomain --create #{subdomain} -domain #{domain} -ssi true -php true  -www_root #{path}`
+end
+
+def remove_subdomain_plesk(subdomain)
+  domain = "memberhive.com"
+  `sudo /usr/local/psa/bin/subdomain --remove #{subdomain} -domain #{domain}`
+end
