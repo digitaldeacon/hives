7395b15c118f4e5af456312e45c4e47c8b9a1f71
Paul Walger <metaxy@walger.name>
2015-07-11 18:18:30 +0200

    update

--- update.rb
+++ update.rb
@@ -1,4 +1,4 @@
 require_relative 'common'
 #FileUtils.cp_r(path_dist(), path_subdomain("static"))
 
-`cd ${path}/data/code && git pull origin master`
+`cd #{path}/data/code && git pull origin master`
