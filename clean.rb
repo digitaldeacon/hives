71da4c4a10d55dc0ac7ad6704606c73bf134a4fe
Paul Walger <metaxy@walger.name>
2015-08-06 15:24:10 +0200

    fast kill

--- clean.rb
+++ clean.rb
@@ -5,8 +5,8 @@ require_relative 'common'
 if $config_local.has_key? "sites"
   $config_local["sites"].each do |name, config|
     #remove_subdomain_plesk(name)
-    exe("docker stop #{config['docker_server_name']} && docker rm #{config['docker_server_name']}")
-    exe("docker stop #{config['docker_db_name']} && docker rm #{config['docker_db_name']}")
+    exe("docker stop -t 1 #{config['docker_server_name']} && docker rm #{config['docker_server_name']}")
+    exe("docker stop -t 1 #{config['docker_db_name']} && docker rm #{config['docker_db_name']}")
     exe("slc ctl remove #{name}")
   end
 end
