ec9e61c744b1d8410a0f80d0974dc1f573b6ad0d
Paul Walger <metaxy@walger.name>
2015-08-08 09:34:29 +0200

    root password

--- common.rb
+++ common.rb
@@ -90,7 +90,7 @@ def create_slc_service(name)
 end
 def set_slc_service(name)
   config = $config_local['sites'][name]
-  exe("slc ctl -C http://127.0.0.1:#{config['deploy_port']} env-set #{name} NODE_ENV=production MH_DB_PASSWORD=#{config['db_password']} MH_DB_NAME=#{name} MH_DB_USER=#{name} STRONGLOOP_CLUSTER=1")
+  exe("slc ctl -C http://127.0.0.1:#{config['deploy_port']} env-set #{name} NODE_ENV=production MH_DB_PASSWORD=#{config['db_password']} MH_DB_NAME=#{name} MH_DB_USER=#{name} MH_ROOT_EMAIL='#{config['root_email']}' MH_ROOT_PASSWORD=#{config['root_password']} STRONGLOOP_CLUSTER=1")
   exe("slc ctl -C http://127.0.0.1:#{config['deploy_port']} set-size #{name} 1")
 end
 def build_docker()
--- create.rb
+++ create.rb
@@ -2,16 +2,21 @@ require 'json'
 require 'fileutils'
 require_relative 'common'
 
-def create_site(name)
+def create_site(name, config)
   puts "creating site #{name}".blue
+  
   if(not subdomain_exists? name)
     create_subdomain_plesk(name)
   end
+  
   deploy_port = $config_local.fetch('deploy_port', 8701)
   web_port = $config_local.fetch('web_port', 10000)
   docker_server_name = "mh-server-"+name
   docker_db_name = "mh-db-"+name
   db_password = rand(36**12).to_s(36)
+  root_password = rand(36**12).to_s(36)
+  root_email = config['root-email'] || name + "@memberhive.com"
+      
   if(not $config_local.has_key? 'sites')
     $config_local['sites'] = {}
   end
@@ -23,6 +28,7 @@ def create_site(name)
         "docker_server_name" => docker_server_name,
         "docker_db_name" => docker_db_name,
         "db_password" => db_password
+        "root_password" => root_password
       }
   $config_local['deploy_port'] = deploy_port+1;
   $config_local['web_port'] = web_port+1;
@@ -53,7 +59,7 @@ def main()
   $config_local['docker_build'] = true
   $config["sites"].each do |name, config|
     if not $config_local.has_key? "sites" or not $config_local["sites"].has_key? name
-      create_site(name)
+      create_site(name, config)
     end
   end
   write_local_config()
