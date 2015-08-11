52ea2142d4787eb2f6c86eb71afa00bfbfa16062
Paul Walger <metaxy@walger.name>
2015-08-11 11:00:03 +0200

    create path for avatar

--- common.rb
+++ common.rb
@@ -73,6 +73,7 @@ def create_server_docker(name, docker_server_name, deploy_port, web_port, db_nam
   puts "Create docker server for #{name}".blue
   files_path = files_path(name)
   FileUtils.mkpath files_path 
+  FileUtils.mkpath(files_path+'/avatar')
   exe("docker run -d -p #{deploy_port}:8701 -p #{web_port}:3001 -v #{files_path}:/var/data/files --name #{docker_server_name} --link #{db_name}:db mh-strong-pm")
 end
 
