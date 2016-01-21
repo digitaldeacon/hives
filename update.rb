a7bfedfde149baf12403e63415155a67c7c0a1c9
Paul Walger <metaxy@walger.name>
2016-01-21 18:24:49 +0100

    use docs from git

--- update.rb
+++ update.rb
@@ -5,7 +5,7 @@ puts "------ Update Scripts ---------"
 
 def update_doc()
   FileUtils.rm_rf(path_subdomain("client-docs"))
-  FileUtils.ln_s("#{$path}/data/dist/docs/", path_subdomain("client-docs"))  
+  FileUtils.ln_s("#{$path}/data/code/dist/docs/", path_subdomain("client-docs"))  
 end
 def main()
   update_doc()
