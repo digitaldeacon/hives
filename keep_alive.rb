acacef246f19d889f9ceadaa99f682ae16741ad4
Paul Walger <metaxy@walger.name>
2016-03-30 21:02:11 +0200

    better keepalive by adding to redeploy app

--- keep_alive.rb
+++ keep_alive.rb
@@ -2,15 +2,22 @@ require_relative 'status_common'
 
 def main()
   $config_local["sites"].each do |name, config|
+    fine = true
     if(!server_running(name))
       remove_docker_server(name)
       create_server_docker(name)
+      fine = false
     end
     if(!db_running(name))
       remove_docker_db(name)
       create_server_db(name)
+      fine = false
     end
-   
+    if(!fine)
+      create_slc_service(name)
+      set_slc_service(name)
+      update_server(name)
+    end
   end
 end
 
