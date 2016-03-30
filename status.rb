5e350120eac0ac5796ad7bd3687c35ed57440100
Paul Walger <metaxy@walger.name>
2016-03-30 17:59:30 +0200

    add status tool

---
+++ status.rb
@@ -0,0 +1,38 @@
+require_relative 'common'
+#
+puts "------ Restart all stuff ---------"
+
+
+def main()
+  $config_local["sites"].each do |name, config|
+    puts "Checking Status of #{name}".blue
+    if(server_running(name))
+      puts "[#{name}] Server ok".green
+    else
+      puts "[#{name}] no docker instance".red
+    end
+    
+    if(db_running(name))
+      puts "[#{name}] DB ok".green
+    else
+      puts "[#{name}] DB not Running".red
+    end
+    
+  end
+end
+
+def server_running(name)
+  config = $config_local['sites'][name]
+  exe("docker top #{config['docker_server_name']}")
+end
+def db_running(name)
+  config = $config_local['sites'][name]
+  exe("docker top #{config['docker_db_name']}")
+end
+def db_responding(name)
+end
+def server_responding(name)
+end
+
+
+main()
